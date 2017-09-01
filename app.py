2b081ef240124fb16b4046a1fb74d68d82a46de7
Aditya <adi1998.2010@gmail.com>
2017-09-01 23:40:26 +0530

    yay

--- app.py
+++ app.py
@@ -1,10 +1,12 @@
-from flask import Flask
+from flask import Flask, jsonify
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
-    return "finally working :)"
+    return jsonify(
+        spam = "1"
+    )
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
