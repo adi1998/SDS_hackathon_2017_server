6e993f439add89ae2437deabe16965bae2f292ac
Aditya <adi1998.2010@gmail.com>
2017-09-01 23:23:40 +0530

    hope it works

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 from flask import Flask
-
+from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
-def index():
-	return "lul"
+def homepage():
+    return "lul"
 
-app.run()
+if __name__ == '__main__':
+    app.run(debug=True, use_reloader=True)
+
