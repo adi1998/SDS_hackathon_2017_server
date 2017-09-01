1666331e4c640b03afeb8fc6f7235b20fa076c3f
Aditya <adi1998.2010@gmail.com>
2017-09-02 01:06:59 +0530

    Add model.dat and vect.dat

--- app.py
+++ app.py
@@ -1,6 +1,18 @@
 from flask import *
-from datetime import datetime
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.naive_bayes import MultinomialNB , 
+from pickle import load
+
+import pandas as pd
+import numpy as np
+
+
 app = Flask(__name__)
+
+def preprocess_data(data):
+	data = data.replace('\d+',' ')
+	data = data.replace('[^a-zA-Z0-9 \n]', ' ')
+	return data
 
 @app.route('/',methods = ['GET', 'POST'])
 def homepage():
@@ -8,13 +20,18 @@ def homepage():
 	if not data:
 		return jsonify(success = 0)
 
-	#data = preprocess_data(data)
-
-	# get from trained model
-	result = 0 
+	data = preprocess_data(data)
+	
+	df = pd.DataFrame({'Message':data})
+	print df
+	
+	vect = load(file('vect.dat'))
+	model = load(file('model.dat'))
+	
+	result = model.predict(vect.transform(df))
+	print result
 	
 	return jsonify(spam = result, success = 1)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
-
