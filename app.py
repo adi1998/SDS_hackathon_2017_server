a2e33a4dc200d4d69a73b094a61a9cad892b3eb1
Aditya <adi1998.2010@gmail.com>
2017-09-01 23:51:28 +0530

    handle empty request

--- app.py
+++ app.py
@@ -4,10 +4,13 @@ app = Flask(__name__)
 
 @app.route('/',methods = ['GET', 'POST'])
 def homepage():
-	data = request.args.get('data')
+	try:
+		data = request.args.get('data')
+	except:
+		return jsonify(success = 0)
 	#data = preprocess_data(data)
 	result = 0 # get from trained model
-	return jsonify(spam = result)
+	return jsonify(spam = result, success = 1)
 
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
