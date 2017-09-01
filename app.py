580e5776773da330d37a26a1a07eb57847a32d9d
Aditya <adi1998.2010@gmail.com>
2017-09-01 23:48:29 +0530

    fix indent

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ def homepage():
 	data = request.args.get('data')
 	#data = preprocess_data(data)
 	result = 0 # get from trained model
-    return jsonify(spam = result)
-    
+	return jsonify(spam = result)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
